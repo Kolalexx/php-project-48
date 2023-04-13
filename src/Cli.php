424a8f8a4b6b21c2e3e916f554c5b83fe1505436
Aleksei Kolesnikov <kolalexx@yandex.ru>
2023-04-13 05:22:50 +0300

    fixed gendiff

--- src/Cli.php
+++ src/Cli.php
@@ -14,10 +14,12 @@ function run()
     Usage:
         gendiff (-h|--help)
         gendiff (-v|--version)
+        gendiff [--format <fmt>] <firstFile> <secondFile>
 
     Options:
         -h --help                     Show this screen
         -v --version                  Show version
+        --format <fmt>                Report format [default: stylish]
     DOC;
     
     $args = Docopt::handle($doc, array('version'=>'1.0'));
