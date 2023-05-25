9a08d588ff26a4ad5beca90d5ae6e793be313f6d
Aleksei Kolesnikov <kolalexx@yandex.ru>
2023-05-25 22:28:37 +0300

    added test flat yaml files

--- tests/ComparisonTest.php
+++ tests/ComparisonTest.php
@@ -7,7 +7,7 @@ use function Differ\Comparison\gendiff;
 
 class ComparisonTest extends TestCase
 {
-    public function testGendiff1(): void
+    public function testGendiffJson1(): void
     {
         $file1 = 'tests/fixtures/file1.json';
         $file2 = 'tests/fixtures/file2.json';
@@ -17,7 +17,7 @@ class ComparisonTest extends TestCase
         gendiff($file1, $file2, 'stylish');
     }
 
-    public function testGendiff2(): void
+    public function testGendiffJson2(): void
     {
         $file3 = 'tests/fixtures/file3.json';
         $file2 = 'tests/fixtures/file2.json';
@@ -27,7 +27,7 @@ class ComparisonTest extends TestCase
         gendiff($file3, $file2, 'stylish');
     }
 
-    public function testGendiff3(): void
+    public function testGendiffJson3(): void
     {
         $file3 = 'tests/fixtures/file3.json';
         $file4 = 'tests/fixtures/file4.json';
@@ -37,7 +37,7 @@ class ComparisonTest extends TestCase
         gendiff($file3, $file4, 'stylish');
     }
 
-    public function testGendiff4(): void
+    public function testGendiffJson4(): void
     {
         $file1 = 'tests/fixtures/file1.json';
         $file5 = 'tests/fixtures/file5.json';
@@ -47,10 +47,60 @@ class ComparisonTest extends TestCase
         gendiff($file1, $file5, 'stylish');
     }
 
-    public function testGendiff5(): void
+    public function testGendiffJson5(): void
     {
         $file1 = 'tests/fixtures/file1.json';
         $file6 = 'tests/fixtures/file6.json';
+
+        $result5 = file_get_contents('tests/fixtures/result5.json', true);
+        $this->expectOutputString($result5);
+        gendiff($file1, $file6, 'stylish');
+    }
+
+    public function testGendiffYaml1(): void
+    {
+        $file1 = 'tests/fixtures/file1.yaml';
+        $file2 = 'tests/fixtures/file2.yaml';
+
+        $result = file_get_contents('tests/fixtures/gendiffFlat.json', true);
+        $this->expectOutputString($result);
+        gendiff($file1, $file2, 'stylish');
+    }
+
+    public function testGendiffYaml2(): void
+    {
+        $file3 = 'tests/fixtures/file3.yaml';
+        $file2 = 'tests/fixtures/file2.yaml';
+
+        $result2 = file_get_contents('tests/fixtures/result2.json', true);
+        $this->expectOutputString($result2);
+        gendiff($file3, $file2, 'stylish');
+    }
+
+    public function testGendiffYaml3(): void
+    {
+        $file3 = 'tests/fixtures/file3.yaml';
+        $file4 = 'tests/fixtures/file4.yaml';
+
+        $result3 = file_get_contents('tests/fixtures/result3.json', true);
+        $this->expectOutputString($result3);
+        gendiff($file3, $file4, 'stylish');
+    }
+
+    public function testGendiffYaml4(): void
+    {
+        $file1 = 'tests/fixtures/file1.yaml';
+        $file5 = 'tests/fixtures/file5.yaml';
+
+        $result4 = file_get_contents('tests/fixtures/result4.json', true);
+        $this->expectOutputString($result4);
+        gendiff($file1, $file5, 'stylish');
+    }
+
+    public function testGendiffYaml5(): void
+    {
+        $file1 = 'tests/fixtures/file1.yaml';
+        $file6 = 'tests/fixtures/file6.yaml';
 
         $result5 = file_get_contents('tests/fixtures/result5.json', true);
         $this->expectOutputString($result5);
