4441135b7e1297ce2e454a0916ba08558013875d
Aleksei Kolesnikov <kolalexx@yandex.ru>
2023-06-20 21:59:10 +0300

    fixed Cli.php

--- src/Cli.php
+++ src/Cli.php
@@ -6,23 +6,22 @@ use Docopt;
 
 use function Differ\Differ\gendiff;
 
+const DOC = <<<EOF
+gendiff -h
+
+Usage: gendiff [options] <firstFile> <secondFile>
+
+Compares two configuration files and shows a difference.
+
+Options:
+    -h, --help                     display help for command
+    -v, --version                  output the version number
+    -f, --format [type]            output format [default: "stylish"]
+EOF;
+
 function run()
 {
-    $doc = <<<DOC
-    gendiff -h
-
-    Usage: gendiff [options] <firstFile> <secondFile>
-
-    Compares two configuration files and shows a difference.
-
-    Options:
-        -h, --help                     display help for command
-        -v, --version                  output the version number
-        -f, --format [type]            output format [default: "stylish"]
-    DOC;
-
-    $args = Docopt::handle($doc, array('version' => '1.0'));
-
+    $args = Docopt::handle(DOC);
     $arg1 = $args['<firstFile>'];
     $arg2 = $args['<secondFile>'];
     $format = $args['--format'];
