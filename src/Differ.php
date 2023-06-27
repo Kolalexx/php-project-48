766a40a19c1f1ba6648ec066da18c3f38410950f
Aleksei Kolesnikov <kolalexx@yandex.ru>
2023-06-27 22:20:56 +0300

    fixed parsers

--- src/Differ.php
+++ src/Differ.php
@@ -15,12 +15,31 @@ function getAbsolutePath(string $path)
     return $realPath;
 }
 
+function readFile(string $pathToFile)
+{
+    $str = file_get_contents($pathToFile);
+    if ($str === '') {
+        return [];
+    }
+    if ($str === false) {
+        throw new \Exception("Cannot parse the file");
+    }
+    return $str;
+}
+
+function getContentAndParse(string $pathToFile)
+{
+    $absolutePath = getAbsolutePath($pathToFile);
+    $fileToString = readFile($absolutePath);
+    $extension = pathinfo($absolutePath, PATHINFO_EXTENSION);
+    $fileToArray = parseToArray($fileToString, $extension);
+    return $fileToArray;
+}
+
 function genDiff(string $path1, string $path2, string $format = 'stylish')
 {
-    $absolutePath1 = getAbsolutePath($path1);
-    $absolutePath2 = getAbsolutePath($path2);
-    $fileArray1 = parseToArray($absolutePath1);
-    $fileArray2 = parseToArray($absolutePath2);
+    $fileArray1 = getContentAndParse($path1);
+    $fileArray2 = getContentAndParse($path2);
     $diff = getDiff($fileArray1, $fileArray2);
     return formatDiff($diff, $format);
 }
--- src/Parsers.php
+++ src/Parsers.php
@@ -4,23 +4,17 @@ namespace Differ\Parsers;
 
 use Symfony\Component\Yaml\Yaml;
 
-function parseToArray(string $pathToFile)
+function parseToArray(string $fileToString, string $extension)
 {
-    $str = file_get_contents($pathToFile);
-    if ($str === '') {
-        return [];
-    }
-    if ($str === false) {
-        throw new \Exception("Cannot parse the file");
-    }
-    if (pathinfo($pathToFile, PATHINFO_EXTENSION) === 'json') {
-        $array = json_decode($str, true);
-    } elseif (pathinfo($pathToFile, PATHINFO_EXTENSION) === 'yaml') {
-        $array = Yaml::parse($str);
-    } elseif (pathinfo($pathToFile, PATHINFO_EXTENSION) === 'yml') {
-        $array = Yaml::parse($str);
-    } else {
-        throw new \Exception("Unknown extension");
+    switch ($extension) {
+        case 'json':
+            $array = json_decode($fileToString, true);
+        case 'yaml':
+            $array = Yaml::parse($fileToString);
+        case 'yml':
+            $array = Yaml::parse($fileToString);
+        default:
+            throw new \Exception("Unknown extension: '{$extension}'");
     }
     return $array;
 }
