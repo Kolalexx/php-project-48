3f8edd389c2d53567e4140f2b9e7ebb794f62be5
Aleksei Kolesnikov <kolalexx@yandex.ru>
2023-06-30 18:59:45 +0300

    fixed formatters in json

--- src/Formatters/Json.php
+++ src/Formatters/Json.php
@@ -4,6 +4,5 @@ namespace Differ\Formatter\Json;
 
 function formatDiffInJson(array $diff): string
 {
-    $json = json_encode($diff, JSON_PRETTY_PRINT);
-    return $json;
+    return json_encode($diff, JSON_PRETTY_PRINT);
 }
